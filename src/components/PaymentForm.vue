4621fe812181e3e423aac798732fd9480453d52a
Marina <churmika@gmail.com>
2021-05-17 23:59:45 +0300

    lesson 3

---
+++ src/components/PaymentForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="date" placeholder="01.01.1900" v-model="date" />
+    <select v-model="category"> 
+      <option v-for="(value, index) in values" :key="index">{{ value }}</option>
+    </select>
+    <input placeholder="Price" v-model.number="price" />
+    <button @click="save">Save</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: '',
+      category: '',
+      price: 0,
+      values: ['Food', 'Transport', 'Education', 'Clothes', 'Others']
+    }
+  },
+  props: {
+    items: Array
+  },
+  methods: {
+    save () {
+      const { date, category, price } = this
+      this.$emit('add', { date, category, price })
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
---
+++ src/components/PaymentsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-for="(item, index) in items" :key="index" class="table_row">
+      <p class="table_cell">{{ item.date }}</p> 
+      <p class="table_cell">{{ item.category }}</p> 
+      <p class="table_cell">{{ item.price }}</p> 
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: Array
+  },
+  methods: {
+    doSomething () {
+      console.log(this.items)
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+ .table_row {
+   margin: 0 auto;
+   width: 360px;
+   display: flex;
+   justify-content: space-around;  
+ }
+
+ .table_cell {
+   width: 60px;
+ }
+</style>
